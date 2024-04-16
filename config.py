e242b274902cf7fa2d8fb3a8bdf38bf58adae973
KAGURA3301 <167175999+KAGURA3301@users.noreply.github.com>
2024-04-16 15:47:18 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cv8u2igkg0ve.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Aacs3253"
+customdb = "employee"
+custombucket = "simkaian-employee"
+customregion = "us-east-1"
+
